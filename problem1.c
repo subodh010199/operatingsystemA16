d930919568a8557050c993e0f637ec75a3e26cbe
subodh010199 <subodhbarthwal008@gmail.com>
2018-03-26 18:04:35 +0530

    3rd revision

--- problem1.c
+++ problem1.c
@@ -1,5 +1,27 @@
-#include<unistd.h>
-#include<stdio.h>
+
+#include<iostream>
+//#include<stdio>
+//#include<conio>
+using namespace std;
 int main(){
-printf("using github");
+int **a,n;
+cout<<"Enter no of processes.";
+cin>>n;
+a=new int*[n];
+for(int i=0;i<n;i++){
+	a[i]=int new[3];
+}
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<3;j++)
+cin>>a[i][j]<<"      ";
+cout<<endl;
+}
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<3;j++)
+cout<<a[i][j]<<"      ";
+cout<<endl;
+}
+
 }
