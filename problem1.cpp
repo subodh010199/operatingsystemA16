1e7206fd56037efed16737b69577de57c8075b06
subodh010199 <32438710+subodh010199@users.noreply.github.com>
2018-04-06 19:18:12 +0530

    Add files via upload

--- problem1.cpp
+++ problem1.cpp
@@ -1,105 +1,136 @@
 
+
 #include<iostream>
 #include<stdlib.h>
-
-//#include<stdio>
-//#include<conio>
+#include<algorithm>
 using namespace std;
-class queue
+int cp[10],time_quantum,wait_time=0,turnaround_time=0,total_time=0;
+int **a,n,tat=0,*rt,q1[10],q2[10],q3[10],q4[10],rear1,rear2,front1=0,front2=0,q5[10],front3=0,rear3=0;
+void swap(int *xp, int *yp)
 {
-              int queue1[5];
-              int rear,front;
-      public:
-              queue()
-                {
-                     rear=-1;
-                     front=-1;
-                }
-              void insert(int x)
-               {
-                   if(rear >  4)
-                    {
-                       cout <<"queue over flow";
-                       front=rear=-1;
-                       return;
-                    }
-                    queue1[++rear]=x;
-               }
-              int delet()
-               {
-                   if(front==rear)
-                     {
-                         return 0;
-                     }
-                    else return queue1[++front];
-                }
-              
-};
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
 int main(){
-queue student,teachers;
-int cp[10],time_quantum=5,wait_time=0,turnaround_time=0;
-int **a,n,tat=0,*rt;
-cout<<"Enter no of processes.";
+//int cp[10],time_quantum=5,wait_time=0,turnaround_time=0,total_time=0;
+//int **a,n,tat=0,*rt,q1[10],q2[10],q3[10],q4[10],rear1,rear2,front1=0,front2=0;
+cout<<"---------------FAIR SHARE ROUND ROBIN------------------"<<endl<<endl<<endl;
+cout<<"Enter no of processes-   ";
 cin>>n;
 a=new int*[n];
 rt=new int[n];
 for(int i=0;i<n;i++){
 	a[i]=new int[3];
 }
+int k=0,m=0,l=0,temp,o=0;
 cout<<"Enter 0 for student query and 1 for teachers query. "<<endl;
 for(int i=0;i<n;i++)
 {
 for(int j=0;j<3;j++)
 {
-	if(a[0][0]==1)
-	teachers.insert(i);
-	else
-	student.insert(i);
 if(j==0)
-cout<<"Enter query status for process "<<i+1<<"  ";
+cout<<"Enter query status for process-   "<<i+1<<"  ";
 else if(j==1)
-cout<<"Enter arrival time for process  "<<i+1<<"  ";
+cout<<"Enter arrival time for process-   "<<i+1<<"  ";
 else
-cout<<"Enter burst time for process  "<<i+1<<"  ";
+cout<<"Enter burst time for process-     "<<i+1<<"  ";
 cin>>a[i][j];
-rt[i]=a[i][2];
-}
 cout<<endl;
 }
+q3[l++]=i;
+/*if(a[i][0]==0)
+q1[k++]=i;
+else if(a[i][0]==1)
+q2[m++]=i;
+cout<<endl;*/
+}
+//q4.sort();
 for(int i=0;i<n;i++)
-tat=tat+a[i][2];
-int time,count,remain=n,flag=0;
-for(time=0,count=0;remain!=0;) 
-  { 
-    if(rt[count]<=time_quantum && rt[count]>0) 
-    { 
-      time+=rt[count]; 
-      rt[count]=0; 
-      flag=1; 
-    } 
-    else if(rt[count]>0) 
-    { 
-      rt[count]-=time_quantum; 
-      time+=time_quantum; 
-    } 
-    if(rt[count]==0 && flag==1) 
-    { 
-      remain--; 
-      cout<<count+1<<"   "<<time-a[count][1]<<"    "<<time-a[count][1]-a[count][2]<<endl;
-      //("P[%d]\t|\t%d\t|\t%d\n",count+1,time-at[count],time-at[count]-bt[count]); 
-      wait_time+=time-a[count][1]-a[count][2]; 
-      turnaround_time+=time-a[count][1]; 
-      flag=0; 
-    } 
-    if(count==n-1) 
-      count=0; 
-    else if(a[count+1][1]<=time) 
-      count++; 
-    else 
-      count=0; 
-}
+q4[i]=a[i][1];
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<n-i-1;j++)
+{
+if(q4[j+1]<q4[j])
+{
+	swap(&q4[j],&q4[j+1]);
+	swap(&q3[j],&q3[j+1]);
 }
 
+}
+}
+for(int i=0;i<n;i++)
+{
+	if(a[q3[i]][0]==0)
+	q1[k++]=q3[i];
+	else if(a[q3[i]][0]==1)
+	q2[m++]=q3[i];
+}
+/*for(int i=0;i<n;i++)
+{
+	cout<<q3[i]<<endl;
+}*/
+/*for(int i=0;i<3;i++)
+{
+	cout<<q1[i]<<"   ";
+	cout<<q2[i]<<"   ";
+}*/
+/*for(int i=0;i<n;i++)
+cout<<q3[i]<<endl;*/
+//for(int i=0;i<2;i++)
+//{
+total_time+=a[q3[0]][1]+a[q3[0]][2];
+for(int i=1;i<n;i++)
+{
+if(a[q3[i]][1]>total_time)
+total_time=a[q3[i]][1];
+total_time+=a[q3[i]][2];
+}
+rear1=k;
+rear2=m;
+rt=new int[10];
+for(int i=0;i<n;i++)
+rt[i]=a[i][2];
+int time_quantum1,time_quantum2;
+if(k>0)
+time_quantum1=60/k;
+if(m>0)
+ time_quantum2=60/m;
+int time=0,count,remain1=k,flag=0,remain2=m;
+time=a[q2[0]][1];
+float ttat=0,twat=0;
+cout<<"PRO"<<"	"<<"TAT"<<"	"<<"WT"<<endl;
+for(int i=0;i<m;i++)
+{
+	time=time+(min(time_quantum2,a[q2[i]][2]));
+	if(a[q2[i]][1]+a[q2[i]][2]<=time && a[q2[i]][2]<time_quantum2)
+	{
+	cout<<q2[i]+1<<"	"<<(max(a[q2[i]][1],time-(min(time_quantum2,a[q2[i]][2])))+a[q2[i]][2])-a[q2[i]][1]<<"		"<<((max(a[q2[i]][1],time-(min(time_quantum2,a[q2[i]][2])))+a[q2[i]][2])-a[q2[i]][1])-a[q2[i]][2]<<endl;
+    ttat=ttat+(max(a[q2[i]][1],time-time_quantum2)+a[q2[i]][2])-a[q2[i]][1];
+    twat=twat+((max(a[q2[i]][1],time-time_quantum2)+a[q2[i]][2])-a[q2[i]][1])-a[q2[i]][2];
+	}
+	else 
+		cout<<q2[i]+1<<"		"<<"Process incompleted"<<endl;
+}
+for(int i=0;i<k;i++)
+{
+	time=time+(min(time_quantum1,a[q1[i]][1]));
+	if(a[q1[i]][1]+a[q1[i]][2]<time && a[q1[i]][2]<time_quantum1)
+	{
+	cout<<q1[i]+1<<"	"<<(max(a[q1[i]][1],time-(min(time_quantum1,a[q1[i]][2])))+a[q1[i]][2])-a[q1[i]][1]<<"		"<<((max(a[q1[i]][1],time-(min(time_quantum1,a[q1[i]][2])))+a[q1[i]][2])-a[q1[i]][1])-a[q1[i]][2]<<endl;
+	 ttat=ttat+(max(a[q1[i]][1],time-time_quantum1)+a[q1[i]][2])-a[q1[i]][1];
+    twat=twat+((max(a[q1[i]][1],time-time_quantum1)+a[q1[i]][2])-a[q1[i]][1])-a[q1[i]][2];
+}
+	
+	else 
+		cout<<q1[i]+1<<"		"<<"Process incompleted"<<endl;
+}
+cout<<endl<<endl;
+cout<<"Total TurnAround Time-"<<ttat<<endl;
+cout<<"Total Waiting Time-"<<twat<<endl;
+cout<<"Average TurnAroundTime Time-"<<ttat/3<<endl;
+cout<<"Average Waiting Time-"<<twat/3;
 
-
-
+}
